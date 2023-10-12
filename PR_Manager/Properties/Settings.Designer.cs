c707fffb128473fe217ec80a79649ffce6a402cc
SHELL <south02274653@gmail.com>
2023-10-12 16:03:05 +0900

    フルスクリーンモードの選択肢の追加
    - モード選択をラジオボタンからコンボボックスに変更
    - 本来の意味での「フルスクリーン」モードの追加
    - 従来の「フルスクリーン」を「仮想フルスクリーン」で置き換え
    - 「仮想フルスクリーンモードを使用しない」ベータ機能を削除

--- PR_Manager/Properties/Settings.Designer.cs
+++ PR_Manager/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PR_Manager.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace PR_Manager.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.1.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.2.0")]
         public string ConfigFileVersion {
             get {
                 return ((string)(this["ConfigFileVersion"]));
